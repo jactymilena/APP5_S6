68737d5b232a901d280949daa0b6b52a26acb9c8
jacty <saej3101@usherbrooke.ca>
2023-07-10 09:35:54 -0400

    Code clean up

--- employees_stalker/views.py
+++ employees_stalker/views.py
@@ -3,11 +3,9 @@ from datetime import datetime
 from flask import request, jsonify
 import json
 
-# from models.relai import Relai
 from models.employees_manager import EmployeesManager
 from models.archive import Archive
 import consts
-# from db.database import Database
 
 archive = Archive()
 manager = EmployeesManager()
@@ -32,8 +30,6 @@ def control():
 def post_ids():
     if request.method == 'POST':
         data = json.loads(request.data)
-        print(data['ids'])
         manager.update(data['ids'])
-        # relai.notify(manager.get_employees())
         return jsonify(isError= False, message= "Success", statusCode= 200, data= data), 200
         
