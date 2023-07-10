703b4e8652b3e70c210d119bbcc8a6bb874347ef
jacty <saej3101@usherbrooke.ca>
2023-07-10 11:26:21 -0400

    BLE Beacon scanner connection to server works

--- app/HelloServer/hello/hello.ino
+++ app/HelloServer/hello/hello.ino
@@ -1,12 +1,41 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <BLEEddystoneURL.h>
+#include <BLEEddystoneTLM.h>
+#include <BLEBeacon.h>
 
 const char* ssid = "Saenzros2_4";
-const char* password = "********"; // NE PAS PUSH 
+const char* password = "****"; // NE PAS PUSH 
 const char* serverURL = "http://10.0.0.223:5000/ids";
 
-void setup() {
-  Serial.begin(115200);
+std::vector<std::string> activeIds(10);
+int scanTime = 5; // seconds
+BLEScan *pBLEScan;
+
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{    
+    void onResult(BLEAdvertisedDevice advertisedDevice)
+    {
+    }
+};
+
+void setupBLE() {
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99); // less or equal setInterval value
+}
+
+void setupWifi() {
   WiFi.begin(ssid, password);
   Serial.println("Connexion au WiFi en cours...");
 
@@ -23,19 +52,60 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
-void loop(){
+
+void setup() {
+  Serial.begin(115200);
+  setupWifi();
+  setupBLE();
+}
+
+
+void scanBLEBeacon() {
+  activeIds.clear();
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  int count = foundDevices.getCount();
+  for(int i = 0; i < count; i++) {
+      auto foundDevice = foundDevices.getDevice(i);
+      if (foundDevice.haveManufacturerData())
+      {
+        std::string strManufacturerData = foundDevice.getManufacturerData();
+
+        uint8_t cManufacturerData[100];
+        strManufacturerData.copy((char *)cManufacturerData, strManufacturerData.length(), 0);
+
+        if (strManufacturerData.length() == 25 && cManufacturerData[0] == 0x4C && cManufacturerData[1] == 0x00)
+        {
+          BLEBeacon oBeacon = BLEBeacon();
+          oBeacon.setData(strManufacturerData);
+          activeIds.push_back(oBeacon.getProximityUUID().toString());
+
+          Serial.printf("iBeacon Frame\n");
+          Serial.printf("-------------------------\n");
+          Serial.printf("UUID: %s\n", oBeacon.getProximityUUID().toString().c_str());
+          Serial.printf("-------------------------\n");
+        }
+      }
+  }
+  
+}
+
+void loop() {
+  scanBLEBeacon();
 
  if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
-    // Spécifiez l'URL du serveur et l'endpoint
     http.begin(serverURL);
 
+    String data = "{\"ids\": [";
+    for(int i = 0; i < activeIds.size(); i++) {
+      data += "\"" + String(activeIds[i].c_str());
+      if(i != activeIds.size() - 1) {
+        data += "\",";
+      }
+    }
+    data += "\"]}";
 
-    // Construisez les données à envoyer au serveur
-    String data = "{\"ids\": [\"7f3bfd87-a46d-4a51-bdeb-7a479432a8f8\", \"5b964c48-2edc-43b3-b6bf-c50a563460a2\" ]}";
-
-    // Envoyez une requête POST avec les données
     int httpResponseCode = http.POST(data);
 
     if (httpResponseCode > 0) {
